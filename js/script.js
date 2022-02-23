d088135f07fd0c75b09a4193433368ea51947c30
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-02-23 15:50:37 +0100

    ciclo più istruzioni condizionali con controllo in console

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-for (let i = 0; i <= 100; i++) {
-    document.writeln(i);
-    console.log(i);
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        document.writeln('FizzBuzz');
+        console.log('FizzBuzz');
+    } else if (i % 3 == 0) {
+        document.writeln('Fizz');
+        console.log('Fizz');
+    } else if (i % 5 == 0) {
+        document.writeln('Buzz');
+        console.log('Buzz');
+    } else {
+        document.writeln(i)
+        console.log(i)
+    }
 }
+
