938076dbe9c3457b060bd8970523e096db09e48f
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-02-23 16:27:06 +0100

    html-css e js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
+let squares = document.querySelector('.squares');
+
 for (let i = 1; i <= 100; i++) {
+        let elemento = document.createElement('div');
+        elemento.classList.add('box', 'blue');
+        elemento.innerHTML = i;
     if (i % 3 == 0 && i % 5 == 0) {
-        document.writeln('FizzBuzz');
-        console.log('FizzBuzz');
+        elemento.classList.add('red');
+        elemento.innerHTML = 'FizzBuzz';  
     } else if (i % 3 == 0) {
-        document.writeln('Fizz');
-        console.log('Fizz');
+        elemento.classList.add('green');
+        elemento.innerHTML = 'Fizz';   
     } else if (i % 5 == 0) {
-        document.writeln('Buzz');
-        console.log('Buzz');
-    } else {
-        document.writeln(i)
-        console.log(i)
-    }
+        elemento.classList.add('yellow');
+        elemento.innerHTML = 'Buzz';  
+    } 
+    squares.append(elemento);
 }
+
+
+
+ 
 
